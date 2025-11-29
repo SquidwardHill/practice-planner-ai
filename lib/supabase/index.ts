767d254914239e758bbf7e96299a3dd375df4a87
Sydney Hill <sydneyehill@gmail.com>
2025-11-28 19:53:10 -0500

    more supabase utils

--- lib/supabase/index.ts
+++ lib/supabase/index.ts
@@ -1,3 +1,12 @@
-export { supabase as client } from './client'
-export { supabase as server } from './server'
+// Supabase clients
+export { supabase as client } from "./client";
+export { supabase as server } from "./server";
 
+// Database types
+export * from "./database.types";
+
+// Query helpers
+export * from "./queries";
+
+// Subscription helpers
+export * from "./subscription";
