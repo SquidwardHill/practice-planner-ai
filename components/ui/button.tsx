54da2a4da7f88374b6255263f7b95da19ce8507c
Sydney Hill <sydneyehill@gmail.com>
2026-02-04 02:09:31 -0800

    [style] chadcn imports and updates

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -24,7 +24,7 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3 text-base",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5 text-base",
+        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5 text-sm",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4 text-lg",
         icon: "size-9", // Square icon button (convenience for default-sized icons)
       },
